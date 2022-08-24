007514215891ce487152af205da12cf24a5bbf1b
Aleksei Punov <design.qs@gmail.com>
2022-08-25 02:44:22 +0600

    Update to 0.9.1

--- src/config/hdc.ts
+++ src/config/hdc.ts
@@ -3,11 +3,11 @@ import { readFile, writeFile } from 'fs';
 import { Request, Response } from 'express';
 
 export const PORT = 5000;
-export const PUBLIC_FOLDER = __dirname + '/../../../tmp/recording';
-export const FRAMES_ROOT_FOLDER = __dirname + '/../../../tmp/recording/pic';
-export const GPS_ROOT_FOLDER = __dirname + '/../../../tmp/recording/gps';
-export const IMU_ROOT_FOLDER = __dirname + '/../../../tmp/recording/imu';
-export const LORA_ROOT_FOLDER = __dirname + '/../../../tmp/recording/lora';
+export const PUBLIC_FOLDER = __dirname + '/../../../mnt/data';
+export const FRAMES_ROOT_FOLDER = __dirname + '/../../../mnt/data/pic';
+export const GPS_ROOT_FOLDER = __dirname + '/../../../mnt/data/gps';
+export const IMU_ROOT_FOLDER = __dirname + '/../../../mnt/data/imu';
+export const LORA_ROOT_FOLDER = __dirname + '/../../../mnt/data/lora';
 export const BUILD_INFO_PATH = __dirname + '/../../../etc/version.json';
 export const LED_CONFIG_PATH = __dirname + '/../../../tmp/led.json';
 export const IMAGER_CONFIG_PATH =
@@ -59,7 +59,7 @@ export const updateFirmware = async (req: Request, res: Response) => {
 export const switchToP2P = async (req: Request, res: Response) => {
   try {
     // No need to wait for response — it's tearing down current Network method, so it will kill the connection anyways
-    exec(__dirname + '/switch_P2P.sh');
+    exec(__dirname + '/network/test_P2Pconnect_any.sh');
     res.json({
       output: 'done',
     });
@@ -70,7 +70,7 @@ export const switchToP2P = async (req: Request, res: Response) => {
 
 export const switchToAP = async (req: Request, res: Response) => {
   try {
-    exec(__dirname + '/switch_AP.sh');
+    exec(__dirname + '/network/wifi_switch_AP.sh');
     res.json({
       output: 'done',
     });
