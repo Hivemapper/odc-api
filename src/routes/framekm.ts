156ac50e3d0d1343941b8391b998426925d5e313
Aleksei Punov <design.qs@gmail.com>
2023-01-18 18:05:06 +0400

    Stable download stream and proper handlers to create request and reset the stream

--- src/routes/framekm.ts
+++ src/routes/framekm.ts
@@ -7,6 +7,7 @@ import {
   readdir,
   readdirSync,
   rmSync,
+  statSync,
 } from 'fs';
 import { concatFrames } from 'util/framekm';
 import { exec } from 'child_process';
@@ -36,7 +37,7 @@ router.post('/:name', async (req: Request, res: Response) => {
 
 let i = 0;
 let keepAliveInterval: any = null;
-let mainStream: any = null;
+let fileToDownload = '';
 let isInProgress = false;
 
 router.get('/stream', async (req: Request, res: Response) => {
@@ -48,38 +49,83 @@ router.get('/stream', async (req: Request, res: Response) => {
     mkdirSync(STREAM_REQUEST_FOLDER);
   }
 
-  mainStream = res;
+  console.log('Download pipe opened');
+
+  if (keepAliveInterval) {
+    clearInterval(keepAliveInterval);
+  }
+  isInProgress = false;
+
+  const cleanupDownloadProgress = () => {
+    console.log('Download: finished');
+    isInProgress = false;
+    if (
+      fileToDownload &&
+      existsSync(STREAM_REQUEST_FOLDER + '/' + fileToDownload)
+    ) {
+      try {
+        rmSync(STREAM_REQUEST_FOLDER + '/' + fileToDownload);
+      } catch (e: unknown) {
+        console.log(e);
+      }
+      if (res) {
+        res.write(JSON.stringify({ name: fileToDownload, done: true }));
+      }
+    }
+    fileToDownload = '';
+  };
+
+  res
+    .on('unpipe', cleanupDownloadProgress)
+    .on('error', cleanupDownloadProgress)
+    .on('close', () => {
+      console.log('Download pipe closed');
+      isInProgress = false;
+      clearInterval(keepAliveInterval);
+      keepAliveInterval = null;
+    })
+    .on('finish', () => {
+      console.log('Download pipe finished');
+      isInProgress = false;
+      clearInterval(keepAliveInterval);
+      keepAliveInterval = null;
+    });
 
   try {
     if (!keepAliveInterval) {
+      res.write(JSON.stringify({ keepAlive: i }));
       keepAliveInterval = setInterval(() => {
         if (!isInProgress) {
-          mainStream.write(JSON.stringify({ keepAlive: ++i }));
+          res.write(JSON.stringify({ keepAlive: ++i }));
           readdir(
             STREAM_REQUEST_FOLDER,
             (err: NodeJS.ErrnoException | null, files: string[]) => {
               if (files.length) {
                 isInProgress = true;
                 try {
-                  mainStream.write(
-                    JSON.stringify({ name: files[0], bytes: 0 }),
-                  );
-                  const stream = createReadStream(
-                    FRAMEKM_ROOT_FOLDER + '/' + files[0],
-                  );
-                  stream
-                    .pipe(mainStream, { end: false })
-                    .on('unpipe', () => {
-                      isInProgress = false;
-                      rmSync(STREAM_REQUEST_FOLDER + '/' + files[0]);
-                    })
-                    .on('error', (err: any) => {
-                      res.write(JSON.stringify({ error: err }));
-                      isInProgress = false;
-                      rmSync(STREAM_REQUEST_FOLDER + '/' + files[0]);
-                    });
-                } catch (error) {
-                  res.write(JSON.stringify({ error }));
+                  fileToDownload = files[0];
+
+                  console.log('Download: start: ' + fileToDownload);
+                  if (existsSync(FRAMEKM_ROOT_FOLDER + '/' + fileToDownload)) {
+                    const fileStat = statSync(
+                      FRAMEKM_ROOT_FOLDER + '/' + fileToDownload,
+                    );
+                    res.write(
+                      JSON.stringify({
+                        name: fileToDownload,
+                        bytes: fileStat.size,
+                      }),
+                    );
+                    const stream = createReadStream(
+                      FRAMEKM_ROOT_FOLDER + '/' + fileToDownload,
+                    );
+                    stream.pipe(res, { end: false });
+                  } else {
+                    cleanupDownloadProgress();
+                  }
+                } catch (err: unknown) {
+                  console.log('Download pipe caught error on file read');
+                  res.write(JSON.stringify({ error: err }));
                   isInProgress = false;
                 }
               }
@@ -90,7 +136,32 @@ router.get('/stream', async (req: Request, res: Response) => {
     }
   } catch (error: unknown) {
     i = 0;
+    console.log('Download pipe error');
     res.end(JSON.stringify({ error }));
+  }
+});
+
+router.get('/reset', async (req: Request, res: Response) => {
+  if (keepAliveInterval) {
+    clearInterval(keepAliveInterval);
+  }
+  isInProgress = false;
+  fileToDownload = '';
+  res.json({
+    done: true,
+  });
+});
+
+router.get('/request/:name', async (req: Request, res: Response) => {
+  try {
+    if (req.params.name) {
+      exec('touch ' + STREAM_REQUEST_FOLDER + '/' + req.params.name);
+    }
+    res.json({
+      done: true,
+    });
+  } catch (error) {
+    res.json({ error });
   }
 });
 
