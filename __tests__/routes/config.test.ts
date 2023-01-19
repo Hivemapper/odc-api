bd89b16c85b056007a4f3bcdf8dd21c52b104e24
tohkubo <tomohiro@hivemapper.com>
2023-01-18 20:51:25 -0800

    working jest

---
+++ __tests__/routes/config.test.ts
@@ -0,0 +1,17 @@
+
+
+
+describe('Config handlers', () => {
+
+  describe('PUT /config/resolution', () => {
+
+    it('should return 200 && update resolution with a valid resolution type', async () => {
+
+    })
+
+    it('should return 400 with passed an invalid resolution type', async () => {
+
+    })
+
+  })
+})
