37845f7716cea0965dccada5e081d3d6bba46def
tohkubo <tomohiro@hivemapper.com>
2023-01-20 14:43:23 -0800

    should no longer depend on time not being set

--- src/services/updateCameraConfig.ts
+++ src/services/updateCameraConfig.ts
@@ -3,7 +3,6 @@ import { CAMERA_TYPE, CMD, IMAGER_CONFIG_PATH } from 'config';
 import { writeFile } from 'fs';
 import { CameraType, IService } from 'types';
 import { getCameraConfig } from 'util/index';
-import { ifTimeSet } from 'util/lock';
 
 export const UpdateCameraConfigService: IService = {
   execute: async () => {
@@ -19,22 +18,19 @@ export const UpdateCameraConfigService: IService = {
         {
           encoding: 'utf-8',
         },
-        () => {
-          if (ifTimeSet()) {
-            exec(CMD.STOP_CAMERA, () => {
-              exec(CMD.START_CAMERA, () => {
-                console.log('Successfully restarted the camera');
-              });
-            });
-          } else {
-            // do not restart the camera for image rotation
-            // it will be restarted on 3d lock anyways
-          }
-        },
+        () => restartCamera(),
       );
     } catch (e: unknown) {
       console.log('Camera Config service failed with error', e);
     }
   },
   delay: 200,
+};
+
+const restartCamera = () => {
+  exec(CMD.STOP_CAMERA, () => {
+    exec(CMD.START_CAMERA, () => {
+      console.log('Successfully restarted the camera');
+    });
+  });
 };
