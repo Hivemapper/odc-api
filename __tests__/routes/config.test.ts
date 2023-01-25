28e0c758e0a86f03be18a67ffa10f8326f532eba
tohkubo <tomohiro@hivemapper.com>
2023-01-25 13:39:34 -0800

    Add jest to odc-api

---
+++ __tests__/routes/config.test.ts
@@ -0,0 +1,7 @@
+describe('Camera Configuration endpoints', () => {
+  describe('POST /config/cameraconfig', () => {
+    it('should return 200 with the current camera configurations', async () => {
+      console.log('foobar');
+    });
+  });
+});
