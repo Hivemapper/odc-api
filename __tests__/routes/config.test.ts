00a7a2bb0757966544592d7dc0d1f078b7495eea
tohkubo <tomohiro@hivemapper.com>
2023-01-25 16:46:28 -0800

    get

--- __tests__/routes/config.test.ts
+++ __tests__/routes/config.test.ts
@@ -1,5 +1,5 @@
 describe('Camera Configuration endpoints', () => {
-  describe('POST /config/cameraconfig', () => {
+  describe('GET /config/cameraconfig', () => {
     it('should return 200 with the current camera configurations', async () => {
       console.log('test');
       expect(1).toBe(1);
