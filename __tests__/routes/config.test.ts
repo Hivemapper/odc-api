6accfc3dca0b207085c3ce9b8c587165a8b02eae
tohkubo <tomohiro@hivemapper.com>
2023-01-25 17:24:01 -0800

    wip

--- __tests__/routes/config.test.ts
+++
@@ -1,8 +0,0 @@
-describe('Camera Configuration endpoints', () => {
-  describe('GET /config/cameraconfig', () => {
-    it('should return 200 with the current camera configurations', async () => {
-      console.log('test');
-      expect(1).toBe(1);
-    });
-  });
-});
