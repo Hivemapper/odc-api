f9c6932156d0f0f4fe1d8faa85ada8d839b0e000
Aleksei Punov <design.qs@gmail.com>
2023-02-28 21:42:28 +0400

    ODC API v1.2.3 ACL update

--- compiled/dashcam-api.js
+++ compiled/dashcam-api.js
@@ -121,7 +121,7 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.isDev = exports.API_VERSION = void 0;
 __exportStar(__nccwpck_require__(9020), exports);
-exports.API_VERSION = '1.2.2';
+exports.API_VERSION = '1.2.3';
 const isDev = () => {
     return false;
 };
@@ -36096,7 +36096,7 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.isDev = exports.API_VERSION = void 0;
 __exportStar(__nccwpck_require__(4484), exports);
-exports.API_VERSION = '1.2.2';
+exports.API_VERSION = '1.2.3';
 const isDev = () => {
     return false;
 };
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 export * from './hdc';
-export const API_VERSION = '1.2.2';
+export const API_VERSION = '1.2.3';
 
 export const isDev = () => {
   return false;
