84a170f8cca18e8ed1f949afe4a5415544249603
Aleksei Punov <design.qs@gmail.com>
2023-06-13 14:03:49 -0700

    New location for HDC-S frames

--- src/config/hdc-s.ts
+++ src/config/hdc-s.ts
@@ -6,8 +6,8 @@ import { CameraType } from 'types';
 export const PORT = 5000;
 
 export const PUBLIC_FOLDER = __dirname + '/../../../data/recording';
-export const TMP_PUBLIC_FOLDER = __dirname + '/../../../tmp/public';
-export const FRAMES_ROOT_FOLDER = __dirname + '/../../../data/recording/pic';
+export const TMP_PUBLIC_FOLDER = __dirname + '/../../../tmp/recording';
+export const FRAMES_ROOT_FOLDER = '/tmp/recording/pic';
 export const FRAMEKM_ROOT_FOLDER =
   __dirname + '/../../../data/recording/framekm';
 export const STREAM_REQUEST_FOLDER =
