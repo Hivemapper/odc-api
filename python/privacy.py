86a3010eb46e13f2b05c4129c92f525a9849fb1c
Aleksei Punov <design.qs@gmail.com>
2023-09-01 18:39:37 -0700

    Apply blur to images

--- python/privacy.py
+++ python/privacy.py
@@ -20,17 +20,20 @@ def load_img(image_path, width, height, tensor_type):
 
   img = cv2.imread(image_path)
   img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-  img = cv2.resize(img, (width, height), cv2.INTER_NEAREST)
+  #keep original image for blurring
+  resized_img = cv2.resize(img, (width, height), cv2.INTER_NEAREST)
 
-  img = img / 255.0
-  img = img.transpose(2, 0, 1)
-  tensor = img[np.newaxis, :, :, :].astype(dtype)
+  resized_img = resized_img / 255.0
+  resized_img = resized_img.transpose(2, 0, 1)
+  tensor = resized_img[np.newaxis, :, :, :].astype(dtype)
 
-  return tensor
+  return tensor, img
 
 def detect(image_path, session, width, height, output_names, input_names, tensor_type, conf_threshold, iou_threshold):
-  tensor = load_img(image_path, width, height, tensor_type)
+  tensor, img = load_img(image_path, width, height, tensor_type)
+  start = time.perf_counter()
   outputs = session.run(output_names, {input_names[0]: tensor})
+  inference_time = int((time.perf_counter() - start) * 1000)
   predictions = np.squeeze(outputs[0]).T
 
   # Filter out object confidence scores below threshold
@@ -45,14 +48,46 @@ def detect(image_path, session, width, height, output_names, input_names, tensor
   class_ids = np.argmax(predictions[:, 4:], axis=1)
 
   boxes = predictions[:, :4]
+  boxes = rescale_boxes(boxes, img.shape[1], img.shape[0], width, height)
   boxes = xywh2xyxy(boxes)
-  # Apply non-maxima suppression to suppress weak, overlapping bounding boxes
-  # indices = nms(boxes, scores, self.iou_threshold)
   indices = nms(xywh2xyxy(boxes), scores, iou_threshold)
-  # print(predictions)
-  # print(boxes.tolist())
+  blur_time = blur(img, image_path, boxes, indices)
+  return list(zip(predictions[:, :4][indices].tolist(), scores[indices].tolist(), class_ids[indices].tolist())), inference_time, blur_time 
 
-  return list(zip(predictions[:, :4][indices].tolist(), scores[indices].tolist(), class_ids[indices].tolist()))  
+def blur(img, image_path, boxes, indices):
+  start = time.perf_counter()
+  #Downscale & blur
+  downscale_size = (int(img.shape[1] * 0.2), int(img.shape[0] * 0.2))
+  img_downscaled = cv2.resize(img, downscale_size, interpolation=cv2.INTER_AREA)
+  img_blurred = cv2.GaussianBlur(img_downscaled, (5, 5), 1.5)
+
+  # Upscale
+  upscale_size = (img.shape[1], img.shape[0])
+  img_upscaled = cv2.resize(img_blurred, upscale_size, interpolation=cv2.INTER_LINEAR)
+
+  # Mask from bounding boxes
+  mask = np.zeros((img.shape[0], img.shape[1]), dtype=np.uint8)
+  for i in indices:
+      box = boxes[i].astype(int)
+      cv2.rectangle(mask, (box[0], box[1]), (box[2], box[3]), 255, -1)
+
+  # Composite
+  composite_img = cv2.bitwise_and(img_upscaled, img_upscaled, mask=mask)
+  mask_inv = cv2.bitwise_not(mask)
+  img_unmasked = cv2.bitwise_and(img, img, mask=mask_inv)
+
+  # Save
+  result = cv2.add(composite_img, img_unmasked)
+  result = cv2.cvtColor(result, cv2.COLOR_RGB2BGR)
+  cv2.imwrite(image_path, result)
+  return int((time.perf_counter() - start) * 1000)
+
+def rescale_boxes(boxes, img_width, img_height, model_width, model_height):
+    # Rescale boxes to original image dimensions
+    input_shape = np.array([model_width, model_height, model_width, model_height])
+    boxes = np.divide(boxes, input_shape, dtype=np.float32)
+    boxes *= np.array([img_width, img_height, img_width, img_height])
+    return boxes
 
 def main(input_path, output_path, model_path, tensor_type, conf_threshold, iou_threshold, num_threads):
   session = onnxruntime.InferenceSession(model_path, providers=onnxruntime.get_available_providers())
