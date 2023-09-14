e328d776b7eea308ec26811210aa19bf2c135e70
Aleksei Punov <design.qs@gmail.com>
2023-09-14 14:28:23 -0700

    Yappi profiler, configurable grid depending on input folder size

--- python/privacy.py
+++ python/privacy.py
@@ -8,6 +8,8 @@ import threading
 import time
 from yolov8.utils import nms, xywh2xyxy
 from damoyolo.damoyolo_onnx import DAMOYOLO
+from PIL import Image 
+import yappi
 
 DEFAULT_MODEL_PATH = 'todo'
 CLASS_NAMES = ['face', 'person', 'license-plate', 'car', 'bus', 'truck', 'motorcycle', 'bicycle']
@@ -19,70 +21,162 @@ h2 = int(height/2)
 
 input_names = []
 
-def combine_images(images, folder_path):
-  img = np.zeros((height, width, 3), dtype=np.uint8)
+total_samples = 0
+read_time = 0
+blurred_samples = 0
+downscale_time = 0
+blurring_time = 0
+upscale_time = 0
+combine_time = 0
+inference_time = 0
+mask_time = 0
+composite_time = 0
+save_time = 0
 
-  coords = [(0, 0), (w2, 0), (0, h2), (w2, h2)]
+def readImage(f, w, h): 
+    im = Image.open(f).resize((w, h), Image.NEAREST)
+    return np.asarray(im)
 
-  for i, image_name in enumerate(images):
-      img_path = os.path.join(folder_path, image_name)
-      orig = cv2.imread(img_path)
-      orig_resized = cv2.resize(orig, (w2, h2))
-      x, y = coords[i]
-      img[y:y+h2, x:x+w2] = orig_resized
+def combine_images(images, folder_path, grid_size):
+    
+    w = int(width / grid_size)
+    h = int(height / grid_size)
 
-  return img
+    img = np.zeros((height, width, 3), dtype=np.uint8)
 
-def detect(folder_path, images, session, conf_threshold, nms_threshold):
+    # Create a list of coordinates for the top-left corners of the sub-images
+    coords = [(i * w, j * h) for j in range(grid_size) for i in range(grid_size)]
 
-  # combine images to 2x2 grid & execute
-  img = combine_images(images, folder_path)
+    read_time = 0  # Moved inside the function, you can return it if you need it globally
+
+    # Loop through each image and place it in the grid
+    for i in range(grid_size * grid_size):  # Iterate up to grid_size*grid_size times
+        if i < len(images):  # Check if there's an image to place in the current grid spot
+            image_name = images[i]
+            img_path = os.path.join(folder_path, image_name)
+            start = time.perf_counter()
+            orig_resized = readImage(img_path, w, h)
+            orig_resized = cv2.cvtColor(orig_resized, cv2.COLOR_RGB2BGR)
+            read_time += (time.perf_counter() - start) * 1000
+        else:
+            # Use an empty (black) image for spots without images
+            orig_resized = np.zeros((h, w, 3), dtype=np.uint8)
+        x, y = coords[i]
+        img[y:y+h, x:x+w] = orig_resized
+
+    return img
+
+def transform_box(box, w_offset=0, h_offset=0, width=None, height=None, multiplier=2):
+    # Apply transformations: scale, offset, round
+    new_box = np.floor(np.array([
+        (box[0] - w_offset) * multiplier,
+        (box[1] - h_offset) * multiplier,
+        (box[2] - w_offset) * multiplier,
+        (box[3] - h_offset) * multiplier
+    ])).astype(int)
+    
+    # Making sure all the box coordinates are within the boundaries
+    if width is not None:
+        new_box[::2] = np.clip(new_box[::2], 0, width)  # for x-coordinates
+    if height is not None:
+        new_box[1::2] = np.clip(new_box[1::2], 0, height)  # for y-coordinates
+    
+    return new_box
+
+# 2x2, 3x3, 4x4
+def determine_grid_dimension(num_images):
+    if num_images <= 15:
+        return 2
+    elif num_images <= 30:
+        return 3
+    else:
+        return 4
+
+def determine_image_index(box, w, h, grid_size):
+    x_index = int(box[0] // w)
+    y_index = int(box[1] // h)
+    return y_index * grid_size + x_index
+
+def detect(folder_path, images, session, conf_threshold, nms_threshold, grid_size):
+  global total_samples, blurred_samples, inference_time, combine_time, save_time
+
+  w2 = int(width/grid_size)
+  h2 = int(height/grid_size)
+  
+  # combine images to 4x4 grid & execute
+  start = time.perf_counter()
+  img = combine_images(images, folder_path, grid_size)
+  combine_time += (time.perf_counter() - start) * 1000
+  start = time.perf_counter()
   boxes, scores, class_ids = session(img, nms_th=nms_threshold, score_th=conf_threshold)
+  inference_time += (time.perf_counter() - start) * 1000
 
-  res_output = [[], [], [], []]
+  # draw_img = session.draw(
+  #       img,
+  #       conf_threshold,
+  #       boxes,
+  #       scores,
+  #       class_ids,
+  #       CLASS_NAMES,
+  #       thickness=3,
+  #   )
+  # cv2.imwrite(os.path.join(folder_path, 'combined_' + images[0]), draw_img)
+
+  # Initialize empty output
+  res_output = [[] for _ in range(grid_size*grid_size)]
+
   if len(scores) == 0:
       return res_output
 
-  grouped_boxes = [[], [], [], []]
+  grouped_boxes = [[] for _ in range(grid_size*grid_size)]
+  offsets = [(i * w2, j * h2) for j in range(grid_size) for i in range(grid_size)]
 
-  # split boxes between initial images
+  # Split boxes between initial images
   for box, score, class_id in zip(boxes, scores, class_ids):
-    image_index = 0
-    if box[0] < w2 and box[1] < h2:
-      box = np.array([box[0]*2, box[1]*2, box[2]*2, box[3]*2])
-    elif box[0] >= w2 and box[1] < h2:
-      box = np.array([(box[0] - w2)*2, box[1]*2, (box[2] - w2)*2, box[3]*2])
-      image_index = 1
-    elif box[0] < w2 and box[1] >= h2:
-      box = np.array([box[0]*2, (box[1] - h2)*2, box[2]*2, (box[3] - h2)*2])
-      image_index = 2
-    else:
-      box = np.array([(box[0] - w2)*2, (box[1] - h2)*2, (box[2] - w2)*2, (box[3] - h2)*2])
-      image_index = 3
-    grouped_boxes[image_index].append(box)
-    res_output[image_index].append([CLASS_NAMES[class_id]] + list(box) + [score])
+      image_index = determine_image_index(box, w2, h2, grid_size)
+
+      w_offset, h_offset = offsets[image_index]
+      box = transform_box(box, w_offset, h_offset, width, height, grid_size)
+
+      grouped_boxes[image_index].append(box)
+      res_output[image_index].append([CLASS_NAMES[class_id]] + list(box) + [score])
+
      
   # apply blur
   for i, image_name in enumerate(images):
     if len(grouped_boxes[i]) > 0:
+      blurred_samples += 1
       img_path = os.path.join(folder_path, image_name)
       orig = cv2.imread(img_path)
       result = blur(orig, grouped_boxes[i])
-      cv2.imwrite(os.path.join(folder_path, image_name), result, [int(cv2.IMWRITE_JPEG_QUALITY), 80])
+      start = time.perf_counter()
+      result = cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
+      pil_img = Image.fromarray(result)
+      pil_img.save(os.path.join(folder_path, image_name), quality=80)
+      # cv2.imwrite(os.path.join(folder_path, image_name), result, [int(cv2.IMWRITE_JPEG_QUALITY), 80])
+      save_time += (time.perf_counter() - start) * 1000
 
   return res_output 
 
 def blur(img, boxes):
+  global downscale_time, upscale_time, blurring_time, mask_time, composite_time
   #Downscale & blur
   downscale_size = (int(img.shape[1] * 0.2), int(img.shape[0] * 0.2))
-  img_downscaled = cv2.resize(img, downscale_size, interpolation=cv2.INTER_AREA)
+  start = time.perf_counter()
+  img_downscaled = cv2.resize(img, downscale_size, interpolation=cv2.INTER_NEAREST)
+  downscale_time += (time.perf_counter() - start) * 1000
+  start = time.perf_counter()
   img_blurred = cv2.GaussianBlur(img_downscaled, (5, 5), 1.5)
+  blurring_time += (time.perf_counter() - start) * 1000
 
   # # Upscale
   upscale_size = (img.shape[1], img.shape[0])
-  img_upscaled = cv2.resize(img_blurred, upscale_size, interpolation=cv2.INTER_LINEAR)
+  start = time.perf_counter()
+  img_upscaled = cv2.resize(img_blurred, upscale_size, interpolation=cv2.INTER_NEAREST)
+  upscale_time += (time.perf_counter() - start) * 1000
 
   # Mask from bounding boxes
+  start = time.perf_counter()
   mask = np.zeros((img.shape[0], img.shape[1]), dtype=np.uint8)
   for box in boxes:
     box = box.astype(int)
@@ -90,14 +184,15 @@ def blur(img, boxes):
     if box[2] - box[0] > 0.8 * img.shape[1] and box[1] > 0.5 * img.shape[0]:
       continue
     cv2.rectangle(mask, (box[0], box[1]), (box[2], box[3]), 255, -1)
+  mask_time += (time.perf_counter() - start) * 1000
 
   # Composite
+  start = time.perf_counter()
   composite_img = cv2.bitwise_and(img_upscaled, img_upscaled, mask=mask)
   mask_inv = cv2.bitwise_not(mask)
   img_unmasked = cv2.bitwise_and(img, img, mask=mask_inv)
-
-  # Save
   result = cv2.add(composite_img, img_unmasked)
+  composite_time += (time.perf_counter() - start) * 1000
 
   return result
 
@@ -123,6 +218,7 @@ def main(input_path, output_path, model_path, conf_threshold, nms_threshold, num
 
   metadata = {
     'hash': model_hash,
+    'name': '',
     'inference_time': 0,
     'blurring_time': 0,
     'sample_count': 0,
@@ -130,19 +226,20 @@ def main(input_path, output_path, model_path, conf_threshold, nms_threshold, num
   }
 
   folder_path = input_path
+  grid_dimension = 3 # default grid size
 
   def worker():
     global input_names
     while True:
       images = q.get()
       try:
-        outputs = detect(folder_path, images, session, conf_threshold, nms_threshold)
+        outputs = detect(folder_path, images, session, conf_threshold, nms_threshold, grid_dimension)
         for i, image_name in enumerate(images):
           if len(outputs[i]):
             metadata['detections'][input_names.index(image_name)] = outputs[i]
           
       except Exception as e:
-        print(f"Error processing frame {image_name}. Error: {e}")
+        print(f"Error processing frame {images[0]}. Error: {e}")
       q.task_done()
 
   # init threads
@@ -164,45 +261,93 @@ def main(input_path, output_path, model_path, conf_threshold, nms_threshold, num
 
       if not in_process:  # Only process if not currently in process
           for folder in new_folders:
-              global blurring_time, sample_count, input_names, composite_time, saving_time, inference_time
+              global blurring_time, read_time, total_samples, blurred_samples, blurring_time, combine_time, input_names, composite_time, save_time, inference_time, downscale_time, upscale_time, mask_time
               print('Started processing folder:', folder)
               folder_path = os.path.join(input_path, folder)
               # once new folder discovered, push all the items in the queue
               in_process = True
 
               metadata['start'] = int(time.time()*1000)
+              metadata['name'] = folder
               metadata['detections'] = {}
+              total_samples = 0
+              blurred_samples = 0
+
+              combine_time = 0
+              read_time = 0
+              inference_time = 0
+              blurring_time = 0
+              downscale_time = 0
+              upscale_time = 0
+              mask_time = 0
+              composite_time = 0
+              save_time = 0
+
+              yappi.start()
 
               try:
                 input_names = [f for f in sorted(os.listdir(folder_path)) if f.endswith('.jpg')]
-                metadata['sample_count'] = len(input_names)
+                total_images = len(input_names)
+                metadata['sample_count'] = total_images
+
+                grid_dimension = determine_grid_dimension(total_images)
+                grid_size = grid_dimension * grid_dimension
 
                 i = 0
-                while i < len(input_names):
-                  subset = input_names[i:i+4]
-
-                  # hack for full 2x2 grid. Better do black instead
-                  while (len(subset) < 4):
-                     subset.append(input_names[i])
+                while i < total_images:
+                  subset = input_names[i:i+grid_size]
 
                   q.put(subset) 
-                  i += 4
+                  i += grid_size
 
                 q.join()
+
+                yappi.stop()
+
+                threads = yappi.get_thread_stats()
+                for thread in threads:
+                  print(
+                      "Function stats for (%s) (%d)" % (thread.name, thread.id)
+                  )  # it is the Thread.__class__.__name__
+                  yappi.get_func_stats(ctx_id=thread.id).print_all()
 
                 if not os.path.exists(output_path):
                     os.makedirs(output_path)
                 
                 metadata['end'] = int(time.time()*1000)
-                metadata['inference_time'] = int((metadata['end'] - metadata['start']) / len(input_names))
-                print('Took', int(metadata['inference_time']), 'msecs')
+                total = int(metadata['end'] - metadata['start'])
+                total_samples = len(input_names)
+                print('Took', total, 'msecs')
+
+                all = combine_time + inference_time + downscale_time + upscale_time + blurring_time + mask_time + composite_time + save_time
+                coef = total / all
+
+                # per frame metrics
+                if total_samples > 0:
+                  coef_all = coef / total_samples
+                  metadata['per_frame'] = int(total / len(input_names))
+                  print('Per frame', metadata['per_frame'])
+                  metadata['read_time'] = int(read_time * coef_all)
+                  metadata['combine_time'] = int((combine_time - read_time) * coef_all)
+                  metadata['inference_time'] = int(inference_time * coef_all)
+
+                # blurred frames metrics
+                if blurred_samples > 0:
+                  coef_blurred = coef / blurred_samples
+                  metadata['blurred_count'] = blurred_samples
+                  metadata['downscale_time'] = int(downscale_time * coef_blurred)
+                  metadata['blurring_time'] = int(blurring_time * coef_blurred)
+                  metadata['upscale_time'] = int(upscale_time * coef_blurred)
+                  metadata['mask_time'] = int(mask_time * coef_blurred)
+                  metadata['composite_time'] = int(composite_time * coef_blurred)
+                  metadata['save_time'] = int(save_time * coef_blurred)
 
                 with open(os.path.join(output_path, folder + '.json'), 'w') as f:
                   json.dump(metadata, f, cls=NumpyEncoder)
                 os.rename(folder_path, os.path.join(input_path, 'ready_' + folder))
 
               except Exception as e:
-                print(f"Error processing folder {folder_path}. Error: {e}")
+                print(f"Error processing folder {folder}. Error: {e}")
               in_process = False  # Reset the flag once processing is done
 
       seen_folders.update(new_folders)
@@ -217,6 +362,8 @@ class NumpyEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.float32):
             return float(obj)
+        elif isinstance(obj, np.int64):
+          return int(obj)
         return json.JSONEncoder.default(self, obj)
 
 if __name__ == '__main__':
