7ae9884432e8dffeb04b7ea2545424e4f1fc342a
Aleksei Punov <design.qs@gmail.com>
2023-09-18 18:55:02 -0700

    Latest privacy.py - grids, psutil

--- python/privacy.py
+++ python/privacy.py
@@ -5,11 +5,12 @@ import numpy as np
 import os
 import queue
 import threading
+import shutil
 import time
 from yolov8.utils import nms, xywh2xyxy
 from damoyolo.damoyolo_onnx import DAMOYOLO
 from PIL import Image 
-# import yappi
+import psutil
 
 DEFAULT_MODEL_PATH = 'todo'
 CLASS_NAMES = ['face', 'person', 'license-plate', 'car', 'bus', 'truck', 'motorcycle', 'bicycle']
@@ -18,19 +19,7 @@ width = 2028
 height = 1024
 
 input_names = []
-
-total_samples = 0
-read_time = 0
-blurred_samples = 0
-downscale_time = 0
-blurring_time = 0
-upscale_time = 0
-combine_time = 0
-inference_time = 0
-mask_time = 0
-composite_time = 0
-save_time = 0
-detections = 0
+indexed_names = {}
 
 def readImage(f, w, h): 
   im = Image.open(f).resize((w, h), Image.NEAREST)
@@ -95,31 +84,19 @@ def determine_image_index(box, w, h, grid_size):
   y_index = int(box[1] // h)
   return y_index * grid_size + x_index
 
-def detect(folder_path, images, session, conf_threshold, nms_threshold, grid_size):
-  global total_samples, blurred_samples, inference_time, combine_time, save_time
+def detect(folder_path, images, session_sm, session_md, conf_threshold, nms_threshold, grid_size):
 
   w2 = int(width/grid_size)
   h2 = int(height/grid_size)
   
   # combine images to grid & execute
-  start = time.perf_counter()
   img = combine_images(images, folder_path, grid_size)
-  combine_time += (time.perf_counter() - start) * 1000
-  start = time.perf_counter()
-  boxes, scores, class_ids = session(img, nms_th=nms_threshold, score_th=conf_threshold)
-  inference_time += (time.perf_counter() - start) * 1000
 
-  # Draw the grid to debug
-  # draw_img = session.draw(
-  #       img,
-  #       conf_threshold,
-  #       boxes,
-  #       scores,
-  #       class_ids,
-  #       CLASS_NAMES,
-  #       thickness=3,
-  #   )
-  # cv2.imwrite(os.path.join(folder_path, 'combined_' + images[0]), draw_img)
+  # Execute smaller model over 2x2 grid, larger model over 3x3/4x4 grids
+  session = grid_size == 2 and session_sm or session_md
+  conf = grid_size == 4 and conf_threshold - 0.1 or conf_threshold
+
+  boxes, scores, class_ids = session(img, nms_th=nms_threshold, score_th=conf)
 
   # Initialize empty output
   res_output = [[] for _ in range(grid_size*grid_size)]
@@ -144,38 +121,27 @@ def detect(folder_path, images, session, conf_threshold, nms_threshold, grid_siz
   # apply blur
   for i, image_name in enumerate(images):
     if len(grouped_boxes[i]) > 0:
-      blurred_samples += 1
       img_path = os.path.join(folder_path, image_name)
       orig = cv2.imread(img_path)
       result = blur(orig, grouped_boxes[i])
-      start = time.perf_counter()
       result = cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
       pil_img = Image.fromarray(result)
       pil_img.save(os.path.join(folder_path, image_name), quality=80)
       # cv2.imwrite(os.path.join(folder_path, image_name), result, [int(cv2.IMWRITE_JPEG_QUALITY), 80])
-      save_time += (time.perf_counter() - start) * 1000
 
   return res_output 
 
 def blur(img, boxes):
-  global downscale_time, upscale_time, blurring_time, mask_time, composite_time
   #Downscale & blur
   downscale_size = (int(img.shape[1] * 0.2), int(img.shape[0] * 0.2))
-  start = time.perf_counter()
   img_downscaled = cv2.resize(img, downscale_size, interpolation=cv2.INTER_NEAREST)
-  downscale_time += (time.perf_counter() - start) * 1000
-  start = time.perf_counter()
   img_blurred = cv2.GaussianBlur(img_downscaled, (5, 5), 1.5)
-  blurring_time += (time.perf_counter() - start) * 1000
 
   # # Upscale
   upscale_size = (img.shape[1], img.shape[0])
-  start = time.perf_counter()
   img_upscaled = cv2.resize(img_blurred, upscale_size, interpolation=cv2.INTER_NEAREST)
-  upscale_time += (time.perf_counter() - start) * 1000
 
   # Mask from bounding boxes
-  start = time.perf_counter()
   mask = np.zeros((img.shape[0], img.shape[1]), dtype=np.uint8)
   for box in boxes:
     box = box.astype(int)
@@ -183,44 +149,52 @@ def blur(img, boxes):
     if box[2] - box[0] > 0.8 * img.shape[1] and box[1] > 0.5 * img.shape[0]:
       continue
     cv2.rectangle(mask, (box[0], box[1]), (box[2], box[3]), 255, -1)
-  mask_time += (time.perf_counter() - start) * 1000
 
   # Composite
-  start = time.perf_counter()
   composite_img = cv2.bitwise_and(img_upscaled, img_upscaled, mask=mask)
   mask_inv = cv2.bitwise_not(mask)
   img_unmasked = cv2.bitwise_and(img, img, mask=mask_inv)
   result = cv2.add(composite_img, img_unmasked)
-  composite_time += (time.perf_counter() - start) * 1000
 
   return result
 
 def main(input_path, output_path, model_path, conf_threshold, nms_threshold, num_threads, grid_dimension):
-  global detections
   if not os.path.exists(model_path):
     # default model path
-    model_path = '/opt/dashcam/bin/ml/pvc.onnx'
-  session = DAMOYOLO(
-        model_path,
-        providers=[
-            'CPUExecutionProvider',
-        ],
-    )
+    model_path = '/opt/dashcam/bin/ml'
+
+  session_sm = DAMOYOLO(
+    os.path.join(model_path, 'pvc_sm.onnx'),
+    providers=[
+      'CPUExecutionProvider',
+    ],
+  )
+  session_md = DAMOYOLO(
+    os.path.join(model_path, 'pvc_md.onnx'),
+    providers=[
+      'CPUExecutionProvider',
+    ],
+  )
 
   q = queue.Queue()
 
-  model_hash = ''
-  model_hash_path = model_path + '.hash'
+  model_hash_sm = ''
+  model_hash_path = os.path.join(model_path, 'pvc_sm.onnx.hash')
 
   if os.path.exists(model_hash_path):
       with open(model_hash_path, 'r') as file:
-          model_hash = file.read().strip()
+          model_hash_sm = file.read().strip()
+
+  model_hash_md = ''
+  model_hash_path = os.path.join(model_path, 'pvc_md.onnx.hash')
+
+  if os.path.exists(model_hash_path):
+      with open(model_hash_path, 'r') as file:
+          model_hash_md = file.read().strip()
 
   metadata = {
-    'hash': model_hash,
+    'hash': model_hash_sm,
     'name': '',
-    'inference_time': 0,
-    'blurring_time': 0,
     'sample_count': 0,
     'detections': {},
   }
@@ -228,15 +202,21 @@ def main(input_path, output_path, model_path, conf_threshold, nms_threshold, num
   folder_path = input_path
 
   def worker():
-    global input_names, detections
+    global input_names, indexed_names
     while True:
       images = q.get()
       try:
-        outputs = detect(folder_path, images, session, conf_threshold, nms_threshold, grid_dimension)
+        outputs = detect(folder_path, images, session_sm, session_md, conf_threshold, nms_threshold, grid_dimension)
         for i, image_name in enumerate(images):
           if len(outputs[i]):
-            metadata['detections'][input_names.index(image_name)] = outputs[i]
-            detections += len(outputs[i])
+            if image_name.find('ww') > 0:
+              parts = image_name.split('ww')
+              bundle_name = parts[0]
+              if bundle_name not in metadata['detections']:
+                metadata['detections'][bundle_name] = {}
+              metadata['detections'][bundle_name][indexed_names[image_name]] = outputs[i]
+            else:
+              metadata['detections'][indexed_names[image_name]] = outputs[i]
           
       except Exception as e:
         print(f"Error processing frame {images[0]}. Error: {e}")
@@ -250,6 +230,7 @@ def main(input_path, output_path, model_path, conf_threshold, nms_threshold, num
   try:
     print('Starting watcher')
     in_process = False
+    bundled = False
     seen_folders = set()
 
     if not os.path.exists(input_path):
@@ -261,38 +242,46 @@ def main(input_path, output_path, model_path, conf_threshold, nms_threshold, num
 
       if not in_process:  # Only process if not currently in process
           for folder in new_folders:
-              global blurring_time, read_time, total_samples, blurred_samples, blurring_time, combine_time, input_names, composite_time, save_time, inference_time, downscale_time, upscale_time, mask_time, detections
+              global input_names, indexed_names
               print('Started processing folder:', folder)
+              bundled = folder.endswith('_bundled')
+              indexed_names = {}
               folder_path = os.path.join(input_path, folder)
+
               # once new folder discovered, push all the items in the queue
               in_process = True
 
               metadata['start'] = int(time.time()*1000)
               metadata['name'] = folder
+              metadata['bundled'] = bundled
               metadata['detections'] = {}
-              total_samples = 0
-              blurred_samples = 0
-
-              combine_time = 0
-              read_time = 0
-              inference_time = 0
-              blurring_time = 0
-              downscale_time = 0
-              upscale_time = 0
-              mask_time = 0
-              composite_time = 0
-              save_time = 0
-              detections = 0
-
-              # yappi.start()
 
               try:
                 input_names = [f for f in sorted(os.listdir(folder_path)) if f.endswith('.jpg')]
                 total_images = len(input_names)
+                cur_bundle_name = ''
+                cur_index = 0
+                for f in input_names:
+                  if bundled:
+                    parts = f.split('ww')
+                    bundle_name = parts[0]
+                    if cur_bundle_name != bundle_name:
+                      cur_bundle_name = bundle_name
+                      metadata['detections'][bundle_name] = {}
+                      cur_index = 0
+                    indexed_names[f] = cur_index
+                    cur_index += 1
+                  else: 
+                    indexed_names[f] = input_names.index(f)
                 metadata['sample_count'] = total_images
 
                 grid_dimension = determine_grid_dimension(total_images)
                 grid_size = grid_dimension * grid_dimension
+                print('Total images:', total_images)
+                print('Grid size:', grid_size)
+                model_hash = grid_dimension == 2 and model_hash_sm or model_hash_md
+                metadata['grid_dimension'] = grid_dimension
+                metadata['hash'] = model_hash
 
                 i = 0
                 while i < total_images:
@@ -303,56 +292,50 @@ def main(input_path, output_path, model_path, conf_threshold, nms_threshold, num
 
                 q.join()
 
-                # yappi.stop()
-
-                # threads = yappi.get_thread_stats()
-                # for thread in threads:
-                #   print(
-                #       "Function stats for (%s) (%d)" % (thread.name, thread.id)
-                #   )  # it is the Thread.__class__.__name__
-                #   yappi.get_func_stats(ctx_id=thread.id).print_all()
 
                 if not os.path.exists(output_path):
                     os.makedirs(output_path)
+
+                cpu = psutil.cpu_times()
+                print('User CPU time', cpu.idle)
+                mem = psutil.virtual_memory()
+                print('Used memory', mem.used)
+                swap = psutil.swap_memory()
+                print('Used swap', swap.used) 
                 
                 metadata['end'] = int(time.time()*1000)
-                metadata['num_detections'] = detections
+                metadata['cpu_idle'] = int(cpu.idle)
+                metadata['ram_used'] = int(mem.used / 1024 / 1024)
+                metadata['swap_used'] = int(swap.used / 1024 / 1024)
                 total = int(metadata['end'] - metadata['start'])
-                total_samples = len(input_names)
                 print('Took', total, 'msecs')
 
-                all = combine_time + inference_time + downscale_time + upscale_time + blurring_time + mask_time + composite_time + save_time
-                coef = total / all
-
-                # per frame metrics
-                if total_samples > 0:
-                  coef_all = coef / total_samples
-                  metadata['per_frame'] = int(total / len(input_names))
-                  metadata['read_time'] = int(read_time * coef_all)
-                  metadata['combine_time'] = int((combine_time - read_time) * coef_all)
-                  metadata['inference_time'] = int(inference_time * coef_all)
-
-                # blurred frames metrics
-                if blurred_samples > 0:
-                  coef_blurred = coef / blurred_samples
-                  metadata['blurred_count'] = blurred_samples
-                  metadata['downscale_time'] = int(downscale_time * coef_blurred)
-                  metadata['blurring_time'] = int(blurring_time * coef_blurred)
-                  metadata['upscale_time'] = int(upscale_time * coef_blurred)
-                  metadata['mask_time'] = int(mask_time * coef_blurred)
-                  metadata['composite_time'] = int(composite_time * coef_blurred)
-                  metadata['save_time'] = int(save_time * coef_blurred)
-
-                with open(os.path.join(output_path, folder + '.json'), 'w') as f:
-                  json.dump(metadata, f, cls=NumpyEncoder)
-                os.rename(folder_path, os.path.join(input_path, 'ready_' + folder))
+                if bundled:
+                  submeta = metadata.copy()
+                  for key in metadata['detections']:
+                    d = metadata['detections'][key]
+                    submeta['detections'] = d
+                    with open(os.path.join(output_path, key + '.json'), 'w') as meta_file:
+                      json.dump(submeta, meta_file, cls=NumpyEncoder)
+                else: 
+                  with open(os.path.join(output_path, folder + '.json'), 'w') as meta_file:
+                    json.dump(metadata, meta_file, cls=NumpyEncoder)
 
               except Exception as e:
                 print(f"Error processing folder {folder}. Error: {e}")
+
+              try: 
+                rename_to = os.path.join(input_path, 'ready_' + folder)
+                if os.path.exists(rename_to):
+                  shutil.rmtree(rename_to)
+                os.rename(folder_path, rename_to)
+              except Exception as e:
+                print(f"Error renaming folder {folder}. Possible deleted by another process. Error: {e}")
+
               in_process = False  # Reset the flag once processing is done
 
       seen_folders.update(new_folders)
-      time.sleep(3)
+      time.sleep(2)
   except KeyboardInterrupt:
       print('Watcher stopped by user')
   except Exception as e:
