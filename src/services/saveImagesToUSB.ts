7648008bdb30fa19df43169ea5c7d2ba90bcccfd
HariHive <haripriya@hivemapper.com>
2023-10-03 13:44:38 -0700

    update to async readDir

--- src/services/saveImagesToUSB.ts
+++ src/services/saveImagesToUSB.ts
@@ -1,49 +1,53 @@
 import { IService } from '../types';
-import { DiskUsage } from 'types/motionModel';
-import { readdirSync, existsSync, mkdirSync, rename } from 'fs';
+import { readdirSync, existsSync, mkdirSync, rename, statSync, readdir } from 'fs';
 import { USB_WRITE_PATH } from 'config';
-import { getDateFromUnicodeTimestamp } from '../util';
 
-const diskUsage: DiskUsage = {};
-
-export const getDiskUsage = () => {
-    return diskUsage;
-};
+function countFilesInDirectory(directoryPath: string): number {
+    try {
+        const files = readdirSync(directoryPath);
+        const fileCount = files.filter((file) => {
+            return statSync(`${directoryPath}/${file}`).isFile();
+        }).length;
+        return fileCount;
+    } catch (error) {
+        console.error(`Error counting files in directory '${directoryPath}':`, error);
+        return -1;
+    }
+}
 
 export const SaveImagesToUSB: IService = {
     execute: async () => {
         try {
             const directoryPath = USB_WRITE_PATH;
-            const files = readdirSync(directoryPath);
+            const files = readdir(directoryPath, (err, files) => {
+                files.forEach((file) => {
+                    if (file.endsWith("jpg")) {
 
-            files.forEach((file) => {
-                if (file.endsWith("jpg")) {
-                    const dateFromFileName = getDateFromUnicodeTimestamp(file);
-                    const year = dateFromFileName.getFullYear();
-                    const month = String(dateFromFileName.getMonth() + 1).padStart(2, "0"); // Add 1 because months are 0-based
-                    const day = String(dateFromFileName.getDate()).padStart(2, "0");
+                        const formattedDate = new Date().toISOString().split('T')[0];
 
-                    // Create the formatted date string
-                    const formattedDate = `${year}-${month}-${day}`;
-                    const directoryPath = `${USB_WRITE_PATH}/${formattedDate}`;
+                        const directoryPath = `${USB_WRITE_PATH}/${formattedDate}`;
 
-                    //Check if directory exists for the day on which image was collected
-                    if (!existsSync(directoryPath)) {
-                        try {
-                            mkdirSync(directoryPath, { recursive: true });
-                        } catch (error) {
-                            console.error(`FROM SAVEIMAGESTOUSB SERVICE :::: Error creating directory '${directoryPath}':`, error);
+                        //Check if directory exists for the day on which image was collected
+                        if (!existsSync(directoryPath)) {
+                            try {
+                                mkdirSync(directoryPath, { recursive: true });
+                            } catch (error) {
+                                console.error(`FROM SAVEIMAGESTOUSB SERVICE :::: Error creating directory '${directoryPath}':`, error);
+                            }
                         }
+
+                        //Move the JPG image file to the right directory
+                        const sourcePath = `${USB_WRITE_PATH}/${file}`;
+                        const destinationPath = `${USB_WRITE_PATH}/${formattedDate}/${file}`;
+                        rename(sourcePath, destinationPath, (err) => {
+                            if (err) {
+                                console.error(`FROM SAVEIMAGESTOUSB SERVICE :::: Error moving file: ${err}`);
+                            }
+                        });
                     }
-
-                    //Move the JPG image file to the right directory
-                    const sourcePath = `${USB_WRITE_PATH}/${file}`;
-                    const destinationPath = `${USB_WRITE_PATH}/${formattedDate}/${file}`;
-                    rename(sourcePath, destinationPath, (err) => {
-                        if (err) {
-                            console.error(`FROM SAVEIMAGESTOUSB SERVICE :::: Error moving file: ${err}`);
-                        }
-                    });
+                });
+                if (err) {
+                    console.log("Failed to read directory frm serviceeeeeee");
                 }
             });
         } catch (error) {
