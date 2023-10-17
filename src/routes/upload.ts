61a2c5538eedc3f91d07a77b387d08b0a30ba5c8
HariHive <haripriya@hivemapper.com>
2023-10-16 18:20:46 -0700

    added file size check to prevent multiple uploads

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -1,13 +1,15 @@
 import { UPLOAD_PATH } from '../config';
 import { Router } from 'express';
 import { createWriteStream, existsSync, rmSync } from 'fs';
+const { execSync } = require('child_process');
 
 const router = Router();
 
 router.post('/', (req, res) => {
   try {
     req.pipe(req.busboy); // Pipe it trough busboy
-
+    const fileSize = req?.query?.fileSize;
+    console.log(`From upload route: Uploaded File size: ${fileSize}`);
     req.busboy.on('file', (fieldname, file, data) => {
       const filename = data && data.filename ? data.filename : 'upload.raucb';
       console.log(`Upload of '${filename}' started`);
@@ -15,6 +17,13 @@ router.post('/', (req, res) => {
       try {
         try {
           if (existsSync(UPLOAD_PATH + filename)) {
+            const existingFileSize = parseInt(execSync(`ls -l ${UPLOAD_PATH+filename} | awk '{print $5}'`));
+            console.log(`File size is ${existingFileSize} bytes`);
+            if (fileSize && typeof fileSize === 'string' && parseInt(fileSize) === existingFileSize) {
+              res.json({
+                output: 'done',
+              });
+            }
             rmSync(UPLOAD_PATH + filename)
           }
         } catch (e: unknown) {
