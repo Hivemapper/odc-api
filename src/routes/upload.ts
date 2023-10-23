37d7f8027c4eee9fbefc6c4848d582c0d7820c71
HariHive <haripriya@hivemapper.com>
2023-10-23 15:25:47 -0700

    added code to handle filesize in post request

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -1,15 +1,19 @@
 import { UPLOAD_PATH } from '../config';
 import { Router } from 'express';
-import { createWriteStream, existsSync, rmSync } from 'fs';
+import { createWriteStream, existsSync, mkdirSync, rmSync, statSync } from 'fs';
 const { execSync } = require('child_process');
 
 const router = Router();
+let fileSize: string | undefined;
 
 router.post('/', (req, res) => {
   try {
     req.pipe(req.busboy); // Pipe it trough busboy
-    const fileSize = req?.query?.fileSize;
-    console.log(`From upload route: Uploaded File size: ${fileSize}`);
+
+    req.busboy.on('field', (fieldname, file, data) => {
+      console.log(`Size of upload file is ${file}`);
+      fileSize = file;
+    });
     req.busboy.on('file', (fieldname, file, data) => {
       const filename = data && data.filename ? data.filename : 'upload.raucb';
       console.log(`Upload of '${filename}' started`);
@@ -17,29 +21,43 @@ router.post('/', (req, res) => {
       try {
         try {
           if (existsSync(UPLOAD_PATH + filename)) {
-            const existingFileSize = parseInt(execSync(`ls -l ${UPLOAD_PATH+filename} | awk '{print $5}'`));
-            console.log(`File size is ${existingFileSize} bytes`);
-            if (fileSize && typeof fileSize === 'string' && parseInt(fileSize) === existingFileSize) {
+            const stat = statSync(UPLOAD_PATH + filename);
+            if (fileSize && typeof fileSize === 'string' && stat && parseInt(fileSize) === stat.size) {
               res.json({
                 output: 'done',
               });
             }
-            rmSync(UPLOAD_PATH + filename)
+            else {
+              rmSync(UPLOAD_PATH + filename);
+              const fstream = createWriteStream(UPLOAD_PATH + filename);
+              // Pipe it trough
+              file.pipe(fstream);
+
+              // On finish of the upload
+              fstream.on('close', () => {
+                console.log(`Upload of '${filename}' finished`);
+                res.json({
+                  output: 'done',
+                });
+              });
+            }
+          } else {
+            const fstream = createWriteStream(UPLOAD_PATH + filename);
+            // Pipe it trough
+            file.pipe(fstream);
+
+            // On finish of the upload
+            fstream.on('close', () => {
+              console.log(`Upload of '${filename}' finished`);
+              res.json({
+                output: 'done',
+              });
+            });
           }
         } catch (e: unknown) {
-          console.log('Failed deleting outdated file during upload');
+          console.log('Error from upload route', e);
         }
-        const fstream = createWriteStream(UPLOAD_PATH + filename);
-        // Pipe it trough
-        file.pipe(fstream);
 
-        // On finish of the upload
-        fstream.on('close', () => {
-          console.log(`Upload of '${filename}' finished`);
-          res.json({
-            output: 'done',
-          });
-        });
       } catch (e: unknown) {
         console.log(e);
         res.json({
