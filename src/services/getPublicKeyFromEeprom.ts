574642c50cb6f1409e9840af08b6ab4d71661952
Mazar Farran <mazar@hivemapper.com>
2024-03-06 12:26:20 -0800

    Add check for HDC

--- src/services/getPublicKeyFromEeprom.ts
+++ src/services/getPublicKeyFromEeprom.ts
@@ -1,11 +1,18 @@
 import { exec } from 'child_process';
 import { promisify } from 'util';
 import { promises } from 'fs';
+import { CAMERA_TYPE } from 'config';
+import { CameraType } from 'types';
 
 const awaitableExec = promisify(exec);
 const publicKeyPath = '/tmp/publickey.pem';
 
 export const getPublicKeyFromEeprom = async () => {
+  if (CAMERA_TYPE !== CameraType.Hdc) {
+    console.log(`Failed to fetch public key. Only HDC is supported.`);
+    return '';
+  }
+
   try {
     await awaitableExec(`/opt/dashcam/bin/eeprom_access.py -r -ba 0 -o 101 -l 155 -f ${publicKeyPath}`);
   } catch (error) {
