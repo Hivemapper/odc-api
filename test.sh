9e7cf348ae25140c4f31f3d36de49ba4dd052898
Mazar Farran <mazar@hivemapper.com>
2024-05-16 16:33:50 -0700

    Add separate config

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ echo "Testing odc-api"
 cd compiled
 # mkdir -p lib/binding/napi-v6-darwin-unknown-arm64/
 # cp build/Release/node_sqlite3.node lib/binding/napi-v6-darwin-unknown-arm64/
-node odc-api-hdc.js 2>&1 | tee dashcam-logs.log &
+node odc-api-github-linux-environment.js 2>&1 | tee dashcam-logs.log &
 odc_api_pid=$!
 sleep 10
 
