06f2b72bfc6055bc702d01dc4edceea3cbd7d078
Mazar Farran <mazar@hivemapper.com>
2024-05-15 18:16:07 -0700

    Fix

--- test.sh
+++ test.sh
@@ -3,6 +3,8 @@
 echo "Testing odc-api"
 
 cd compiled
+mkdir -p lib/binding/napi-v6-darwin-unknown-arm64/
+cp build/Release/node_sqlite3.node lib/binding/napi-v6-darwin-unknown-arm64/
 node dashcam-api.js 2>&1 | tee dashcam-logs.log &
 odc_api_pid=$!
 sleep 10
