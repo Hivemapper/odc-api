a07c5786ace8b73a28cab5ed749c2e97d66ce054
Mazar Farran <mazar@hivemapper.com>
2024-05-23 10:28:22 -0700

    Change to use HTTP

---
+++ end-to-end-test/download_test_data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p ./reference/metadata
+mkdir -p ./reference/image
+
+echo "Downloading database"
+wget -O ./reference/data-logger.v1.4.5.db https://hdc-firmware.s3.us-west-2.amazonaws.com/cicd-test/dbs/data-logger.v1.4.5.db
+wget -O ./reference/data-logger.v1.4.5.db-shm https://hdc-firmware.s3.us-west-2.amazonaws.com/cicd-test/dbs/data-logger.v1.4.5.db-shm
+wget -O ./reference/data-logger.v1.4.5.db-wal https://hdc-firmware.s3.us-west-2.amazonaws.com/cicd-test/dbs/data-logger.v1.4.5.db-wal
+
+echo "Downloading metadata"
+wget -O ./reference/metadata/km_20240521_202027_3_0.json https://hdc-firmware.s3.us-west-2.amazonaws.com/cicd-test/reference/metadata/km_20240521_202027_3_0.json
+
+echo "Downloading image"
+wget -O ./reference/image/72.jpg https://hdc-firmware.s3.us-west-2.amazonaws.com/cicd-test/reference/image/72.jpg
