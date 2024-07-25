b695ffd086fec5e8c986bde3f8fddcb10232a9c8
mazar-farran <150729175+mazar-farran@users.noreply.github.com>
2024-07-25 12:07:39 -0700

    Fix USB images not getting deleted in order. (#221)
    Part of a workaround fix for USB circular writes.
    
    Writes the GNSS timestamp to `/tmp/gnss_time.txt` periodically for `usb-write.py` to use.
    
    Ticket: https://linear.app/hivemapper/issue/IMCO-166/investigate-hdc-recording-issue
    
    Related to: https://github.com/Hivemapper/hdc_firmware/pull/258

--- src/util/lock.ts
+++ src/util/lock.ts
@@ -1,4 +1,7 @@
 import { exec, ExecException } from 'child_process';
+import fs from 'fs';
+import { CAMERA_TYPE } from 'config';
+import { CameraType } from 'types';
 
 export const DEFAULT_TIME = 1715027100000; // 2024-05-06, dashcam default time is less than this date. So once the date is bigger, we know that system time is set
 let lockTime = 0;
@@ -15,6 +18,13 @@ export const setTime = () => {
 let gnssTime = 0;
 export const setGnssTime = (_gnssTime: number) => {
   gnssTime = _gnssTime;
+  // Workaround for usb circular write issue on HDC
+  // See https://linear.app/hivemapper/issue/IMCO-166/investigate-hdc-recording-issue
+  if (CAMERA_TYPE === CameraType.Hdc) {
+    fs.writeFile('/tmp/gnss_time.txt', gnssTime.toString(), () => {
+      // Do nothing, best effort and we don't want to block.
+    });
+  }
 }
 
 export const getLatestGnssTime = () => {
