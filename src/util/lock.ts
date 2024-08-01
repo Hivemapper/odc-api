fa3c877a923c70eb5cebef132e7ce6fbe8406d1b
Mazar Farran <mazar@hivemapper.com>
2024-08-01 11:05:59 -0700

    Change to only write gnss offset once

--- src/util/lock.ts
+++ src/util/lock.ts
@@ -5,6 +5,7 @@ import { CameraType } from 'types';
 
 export const DEFAULT_TIME = 1715027100000; // 2024-05-06, dashcam default time is less than this date. So once the date is bigger, we know that system time is set
 let lockTime = 0;
+let gnssOffsetWritten = false;
 
 let timeSet = false;
 export const isTimeSet = () => {
@@ -20,10 +21,10 @@ export const setGnssTime = (_gnssTime: number) => {
   gnssTime = _gnssTime;
   // Workaround for usb circular write issue on HDC
   // See https://linear.app/hivemapper/issue/IMCO-166/investigate-hdc-recording-issue
-  if (CAMERA_TYPE === CameraType.Hdc) {
+  if (CAMERA_TYPE === CameraType.Hdc && !gnssOffsetWritten) {
     const gnssOffset = gnssTime - Date.now();
     fs.writeFile('/tmp/gnss_time.txt', gnssOffset.toString(), () => {
-      // Do nothing, best effort and we don't want to block.
+      gnssOffsetWritten = true;
     });
   }
 }
