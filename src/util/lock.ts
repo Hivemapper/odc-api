75b20d40c3b1c074e37cc894ee7eb18474097a20
mazar-farran <150729175+mazar-farran@users.noreply.github.com>
2024-08-06 10:12:03 -0700

    Write offset to gnss_time.txt instead of time (#223)
    Ticket: https://linear.app/hivemapper/issue/IMCO-166/investigate-hdc-recording-issue
    
    We now write the difference between gnss time and system time to the file. Offset is relatively consistent, so there's no concern anymore that usb-write.py will read a stale gnss_offset.txt.

--- src/util/lock.ts
+++ src/util/lock.ts
@@ -5,6 +5,7 @@ import { CameraType } from 'types';
 
 export const DEFAULT_TIME = 1715027100000; // 2024-05-06, dashcam default time is less than this date. So once the date is bigger, we know that system time is set
 let lockTime = 0;
+let gnssOffsetWritten = false;
 
 let timeSet = false;
 export const isTimeSet = () => {
@@ -20,9 +21,14 @@ export const setGnssTime = (_gnssTime: number) => {
   gnssTime = _gnssTime;
   // Workaround for usb circular write issue on HDC
   // See https://linear.app/hivemapper/issue/IMCO-166/investigate-hdc-recording-issue
-  if (CAMERA_TYPE === CameraType.Hdc) {
-    fs.writeFile('/tmp/gnss_time.txt', gnssTime.toString(), () => {
-      // Do nothing, best effort and we don't want to block.
+  if (CAMERA_TYPE === CameraType.Hdc && !gnssOffsetWritten) {
+    const gnssOffset = gnssTime - Date.now();
+    fs.writeFile('/tmp/gnss_time.txt', gnssOffset.toString(), (err) => {
+      if (!err) {
+        gnssOffsetWritten = true;
+      } else {
+        console.log('Failed to write gnss offset', err);
+      }
     });
   }
 }
